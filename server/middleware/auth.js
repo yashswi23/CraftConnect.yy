b045b85b10248fc1f84d880cfd5540b6d6dd0adf
Yashswi sharma <142489383+yashswi23@users.noreply.github.com>
2025-12-21 23:38:06 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,9 +3,6 @@ export default function (req, res, next) {
   //header se token obtain karo
   const token = req.header('x-auth-token');
   //agar token nai hai error message begho
-  if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' });
-  }
   //token verify karo
   try {
     const decoded = jwt.verify(token, 'hamariSuperSecretKey123');
